06d1042c0fff958f246b445664b14c99dc6640df
itmanagerro <mihai.cornateanu@itmanager.ro>
2016-01-22 13:37:41 +0000

    Config, init and params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,9 @@
 # Class bash_history
-class bash_history inherits bash_history::params
+class bash_history
+(
+  $ssh_username = $::bash_history::params::ssh_username,
+  $ssh_clientip = $::bash_history::params::ssh_clientip,
+)
 {
   include bash_history::config
   include bash_history::service
