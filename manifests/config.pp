0411e75fcce3863f970779236a18480005d7afa3
itmanagerro <mihai.cornateanu@itmanager.ro>
2016-01-25 10:33:45 +0000

    All users should have access to pbh

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,28 +10,28 @@ class bash_history::config inherits bash_history
 
     '/etc/bash.bash_logout':
       ensure  => present,
-      mode    => '0640',
+      mode    => '0644',
       owner   => root,
       group   => root,
       content => template('bash_history/etc/bash.bash_logout.erb');
 
     '/etc/pbh/cd_func.sh':
       ensure  => present,
-      mode    => '0640',
+      mode    => '0644',
       owner   => root,
       group   => root,
       content => template('bash_history/etc/pbh/cd_func.sh.erb');
 
     '/etc/pbh/loghistory_func.sh':
       ensure  => present,
-      mode    => '0640',
+      mode    => '0644',
       owner   => root,
       group   => root,
       content => template('bash_history/etc/pbh/loghistory_func.sh.erb');
 
     '/etc/pbh/puppet.bash_history':
       ensure  => present,
-      mode    => '0640',
+      mode    => '0644',
       owner   => root,
       group   => root,
       content => template('bash_history/etc/pbh/puppet.bash_history.erb');
